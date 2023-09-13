c5463179de19a384f20532bb1ae920ec199998de
vinilope <viniciuszirondi@sptech.school>
2023-09-13 09:52:25 -0300

    Refatoração da calculadora e alteração nos inserts

--- Banco de Dados/bd_vinum.sql
+++ Banco de Dados/bd_vinum.sql
@@ -4,54 +4,69 @@ use bd_vinum;
 
 create table Endereco (
     idEndereco int primary key auto_increment,
-    CEP varchar(8),
+    CEP char(8),
     logradouro varchar(50),
     numeroEndereco varchar(5)
 );
+
+
 
 create table Empresa (
     idEmpresa int primary key auto_increment,
     nomeEmpresa varchar(60),
     inscricaoEstadual varchar(9) not null,
     CNPJ varchar(14) not null unique,
-    codEndereco int not null
-);
-
-create table Contato (
-    idContato int primary key auto_increment,
     telefone varchar(13),
     email varchar(40),
-    codEmpresa int not null
-);
+    fkEndereco int not null,
+    constraint fkEnd foreign key (fkEndereco) 
+        references Endereco(idEndereco)
+) auto_increment = 1000;
+
 
 create table Usuario (
     idUsuario int primary key auto_increment,
     nomeUsuario varchar(40),
     email varchar(80),
     password varchar(40),
-    codEmpresa int not null
+    fkEmpresa int not null,
+    constraint fkEmp foreign key (fkEmpresa)
+        references Empresa(idEmpresa)
 );
+
 
 create table Vinho (
     idVinho int primary key auto_increment,
     categoriaVinho varchar(40),
     nomeMarca varchar(40),
     dataProducao date,
-    codMarca int not null,
-    codEmpresa int not null
+    fkEmpresa int not null,
+    constraint fkEmp3 foreign key (fkEmpresa)
+        references Empresa(idEmpresa)
 );
 
-create table Geladeira (
-    idGeladeira int primary key auto_increment,
-    quatidade varchar(10),
-    codVinho int not null
-);
+
+create table Modulo (
+    idModulo int primary key auto_increment,
+    estoque int,
+    categoriaVinho varchar(40)
+) auto_increment = 100;
+
 
 create table Registro (
     idRegistro int primary key auto_increment,
-    codGeladeira int not null,
-    temperatura decimal (2,1),
+    temperatura float,
     umidade int,
-    horario datetime default current_timestamp
+    horario datetime default current_timestamp,
+    fkModulo int not null,
+    constraint fkMod foreign key (fkModulo)
+        references Modulo(idModulo)
 );
- 
+
+
+select * from Endereco;
+select * from Empresa;
+select * from Usuario;
+select * from Vinho;
+select * from Modulo;
+select * from Registro;
--- Banco de Dados/inserts_vinum.sql
+++ Banco de Dados/inserts_vinum.sql
@@ -1,21 +1,49 @@
-use vinhosprint1;
-
 insert into Endereco (CEP, logradouro, numeroEndereco) values
-    ('00000000', 'logradouro', 000),
-    ('00000000', 'logradouro', 000),
-    ('00000000', 'logradouro', 000),
-    ('00000000', 'logradouro', 000);
-    
-insert into Empresa (nomeEmpresa, inscricaoEstadual,cnpj, codEndereco) values
-    ('empresa', '000000000', '00000000000000', 3),
-    ('empresa', '000000000', '00000000000000', 2),
-    ('empresa', '000000000', '00000000000000', 4),
-    ('empresa', '000000000', '00000000000000', 1);
-    
-insert into Contato (telefone, email, codEmpresa) values
-    ('11999999999', 'exemplo@email.com', 2),
-    ('11999999999', 'exemplo@email.com', 3),
-    ('11999999999', 'exemplo@email.com', 4),
-    ('11999999999', 'exemplo@email.com', 1);
-    
-    
+    ('01001001', 'Praça da Sé', 567),
+    ('01310100', 'Avenida Paulista', 1234),
+    ('01305000', 'Rua Augusta', 782);
+
+insert into Empresa (nomeEmpresa, inscricaoEstadual, CNPJ, telefone, email, fkEndereco) values
+    ('empresa A', '123456789', '12345678000100', '1234567891234', 'empresaa@email.com', 2),
+    ('empresa B', '987654321', '98765432000101', '1234567891234', 'empresab@email.com', 1),
+    ('empresa C', '456789123', '23456789000102', '1234567891234', 'empresac@email.com', 3),
+    ('empresa D', '321654987', '87654321000103', '1234567891234', 'empresad@email.com', 3);
+
+insert into Usuario (nomeUsuario, email, password, fkEmpresa) values
+    ('usuario A', 'usuarioa@email.com', 'senha123', 1000),
+    ('usuario B', 'usuariob@email.com', 'senha123', 1002),
+    ('usuario C', 'usuarioc@email.com', 'senha123', 1002),
+    ('usuario D', 'usuariod@email.com', 'senha123', 1001),
+    ('usuario E', 'usuarioe@email.com', 'senha123', 1001),
+    ('usuario F', 'usuariof@email.com', 'senha123', 1002);
+
+insert into Vinho (categoriaVinho, nomeMarca, dataProducao, fkEmpresa) values
+    ('tinto', 'marca', '2021-09-15', 1002),
+    ('espumante', 'marca', '2021-04-04', 1000),
+    ('rosé', 'marca', '2023-02-23', 1001),
+    ('tinto', 'marca', '2022-11-07', 1001),
+    ('branco', 'marca', '2022-08-28', 1002);
+
+insert into Modulo (estoque, categoriaVinho) values
+    (10, 'espumante'),
+    (10, 'tinto'),
+    (10, 'branco'),
+    (10, 'seco'),
+    (10, 'tinto'),
+    (10, 'rosé'),
+    (10, 'doce'),
+    (10, 'branco'),
+    (10, 'espumante'),
+    (10, 'seco');
+ 
+insert into Registro (temperatura, umidade, horario, fkModulo) values
+    (4.6, 98, current_timestamp, 100),
+    (4.6, 98, current_timestamp, 101),
+    (4.6, 98, current_timestamp, 102),
+    (4.6, 98, current_timestamp, 103),
+    (4.6, 98, current_timestamp, 104),
+    (4.6, 98, current_timestamp, 105),
+    (4.6, 98, current_timestamp, 106),
+    (4.6, 98, current_timestamp, 107),
+    (4.6, 98, current_timestamp, 108),
+    (4.6, 98, current_timestamp, 109);
