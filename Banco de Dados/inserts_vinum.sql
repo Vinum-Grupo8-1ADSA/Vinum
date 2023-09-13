e6ae512924986fa490d0bb623a8c4c2b46235d93
vinilope <viniciuszirondi@sptech.school>
2023-09-12 22:19:57 -0300

    Inserts adicionados

---
+++ Banco de Dados/inserts_vinum.sql
@@ -0,0 +1,21 @@
+use vinhosprint1;
+
+insert into Endereco (CEP, logradouro, numeroEndereco) values
+    ('00000000', 'logradouro', 000),
+    ('00000000', 'logradouro', 000),
+    ('00000000', 'logradouro', 000),
+    ('00000000', 'logradouro', 000);
+    
+insert into Empresa (nomeEmpresa, inscricaoEstadual,cnpj, codEndereco) values
+    ('empresa', '000000000', '00000000000000', 3),
+    ('empresa', '000000000', '00000000000000', 2),
+    ('empresa', '000000000', '00000000000000', 4),
+    ('empresa', '000000000', '00000000000000', 1);
+    
+insert into Contato (telefone, email, codEmpresa) values
+    ('11999999999', 'exemplo@email.com', 2),
+    ('11999999999', 'exemplo@email.com', 3),
+    ('11999999999', 'exemplo@email.com', 4),
+    ('11999999999', 'exemplo@email.com', 1);
+    
+    
