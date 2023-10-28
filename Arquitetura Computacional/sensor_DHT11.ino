70c0dadda6163fdeaa5a637500f601f3e84ad160
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-10-28 12:44:24 -0300

    Adicionado conta no sensor dht11 para simular valores reais

--- Arquitetura Computacional/sensor_DHT11.ino
+++ Arquitetura Computacional/sensor_DHT11.ino
@@ -1,7 +1,7 @@
 #include "DHT.h"
 #define dht_type DHT11 
 
-int dht_pin = A1;
+int dht_pin = A0;
 DHT dht_1 = DHT(dht_pin, dht_type);
 float temperature;
 
@@ -11,20 +11,19 @@ Serial.begin(9600);
 dht_1.begin();
 }
 void loop() {
-  float umidade = dht_1.readHumidity();
-  float temperatura = dht_1.readTemperature();
+  float umidade = 0.3464 * dht_1.readHumidity() + 34.04;
+  float temperatura = 0.0934 * dht_1.readTemperature() + 6.707;
   if (isnan(temperatura) or isnan(umidade)) 
   {
     Serial.println("Erro: Incapaz de ler as informações");
   } 
   else
   {
-    Serial.print("Umidade: ");
+    
     Serial.print(umidade);
-    Serial.print("%; ");
-    Serial.print("Temperatura: ");
-    Serial.print(temperatura);
-    Serial.println("ºC");
+    Serial.print(";");
+    Serial.println(temperatura);
+    
    }
    delay(3000);
    }
