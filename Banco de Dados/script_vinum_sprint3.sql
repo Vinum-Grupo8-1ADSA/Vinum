cf615389e733a8d777f6a4e3abb318ff6fa14bf6
LucasCMendes10 <lucas.mendes@sptech.school>
2023-11-19 16:27:29 -0300

    apagando duplicação de scripts

--- Banco de Dados/script_vinum_sprint3.sql
+++ Banco de Dados/script_vinum_sprint3.sql
@@ -93,56 +93,6 @@ insert into assinaturas values
 (null, 'Castilla', 36, 2399.99),
 (null, 'Domaine', 60, 4999.99);
 
-insert into distribuidora(idDistribuidora, nomeFantasia, razaoSocial, CNPJ, telefone, email, fkAssinaturas) values
-(null, 'Empresa A','Distribuidora A ltda','00000000009301','5511900001111','contato@empresaA.com', 3),
-(null, 'Empresa B','Distribuidora B SA','00000000009401','5511900001112','atendimento@empresab.com', 2),
-(null, 'Empresa C','Distribuidora C ltda','00000000009501','5511900001113','contato@empresaC.com', 1);
-
-insert into produto(idProduto, fkDistribuidora, categoriaVinho, marcaVinho, datasafra, qtdVinho) values
-(null, 1, 'Vinho Tinto', 'Salentein', '2020-02-19', 427),
-(null, 1, 'Vinho Rosé', 'Saurus', '2019-01-22', 320),
-(null, 1, 'Vinho Branco', 'Cocodrilo', '2018-03-21', 174),
-(null, 2, 'Vinho Tinto', 'Salentein', '2021-02-04', 207),
-(null, 2, 'Vinho Rosé', 'Garzon', '2017-01-09', 338),
-(null, 3, 'Vinho Branco', 'Pizzato', '2020-02-21', 142),
-(null, 3, 'Vinho Espumante', 'Ferrari', '2022-03-13', 89);
-
-insert into Adega(idAdega, fkProduto, fkEndereco, fkDistribuidora )values
-(null, 1, 1,'Primeiro Módulo localizado na esquerda do subsolo 1', 1),
-(null, 1, 1,'Segundo Módulo localizado na esquerda do subsolo 1', 1),
-(null, 1, 1,'Terceiro Módulo localizado na esquerda do subsolo 1', 1),
-(null, 1, 1,'Quarto Módulo localizado na esquerda do subsolo 1', 1),
-(null, 1, 1,'Primeiro Módulo localizado na direita do subsolo 1', 1),
-(null, 2, 2,'Segundo Módulo localizado na direita do subsolo 1', 1),
-(null, 2, 2,'Terceiro Módulo localizado na direita do subsolo 1', 1),
-(null, 2, 2,'Quarto Módulo localizado na direita do subsolo 1', 1),
-(null, 2, 2,'Quinto Módulo localizado na direita do subsolo 2', 1),
-(null, 2, 2,'Primeiro Módulo localizado na esquerda do subsolo 2', 1);
-
-insert into sensor(nomeSensor, fkAdega, fkProduto, fkDistribuidora)values
-('Módulo A',1,1,1),
-('Módulo B',2,1,1),
-('Módulo C',3,1,1),
-('Módulo D',4,1,1),
-('Módulo E',5,1,1),
-('Módulo F',6,2,1),
-('Módulo G',7,2,1),
-('Módulo H',8,2,1),
-('Módulo I',9,2,1),
-('Módulo J',10,2,1);
-
-insert into registro(temperatura, umidade, dtHora, fkSensor)values
-(10.42, 66, '2023-10-11 09:58:30',1), 
-(10.41, 67, '2023-10-11 10:00:00',2),
-(10.32, 67, '2023-10-11 11:01:30',3),
-(10.31, 70, '2023-10-11 11:03:00',4),
-(9.98, 72, '2023-10-11 11:04:30',5),
-(10.01, 73, '2023-10-11 11:06:00',6),
-(11.42, 70, '2023-10-11 11:07:30',7),
-(11.72, 68, '2023-10-11 11:09:00',8),
-(11.20, 67, '2023-10-11 11:10:30',9),
-(11.20, 67, '2023-10-11 11:12:00',10);
-    
 -- User com permição de tudo, aquele que vai ser o Servidor    
 create user 'vinumAllGrupo'@'10.18.36.100' identified by 'grupoAll';
 grant all privileges on VinumSprint3.* to 'vinumAllGrupo'@'10.18.36.100';
@@ -157,3 +107,5 @@ flush privileges;
 create user 'vinumSelectGrupo'@'10.18.35.6' identified by 'grupoAll';
 grant select on VinumSprint3.* to 'vinumSelectGrupo'@'10.18.35.6';
 flush privileges;
+
+select * from distribuidora;
